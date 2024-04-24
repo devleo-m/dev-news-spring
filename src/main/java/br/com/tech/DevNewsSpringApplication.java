f2c79f095fe41cb81e507fb2835e13c035b32eb7
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-24 01:05:38 -0300

    first commit

---
+++ src/main/java/br/com/tech/DevNewsSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevNewsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevNewsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/tech/DevNewsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNewsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

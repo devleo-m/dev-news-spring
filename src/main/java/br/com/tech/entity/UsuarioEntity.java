5735832d021f02abdcb6253520b78e34540faa20
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-24 01:31:15 -0300

    Create UsuarioEntity.java

---
+++ src/main/java/br/com/tech/entity/UsuarioEntity.java
@@ -0,0 +1,46 @@
+package br.com.tech.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "artigo")
+public class UsuarioEntity implements Serializable{
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false)
+    private UUID id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    @Column(nullable = true, length = 255)
+    private String biografia;
+
+    @Column(nullable = true, length = 255)
+    private String foto;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDateTime dataCadastro = LocalDateTime.now();
+
+    @Column(name = "ultimo_login")
+    private LocalDateTime ultimoLogin;
+
+}

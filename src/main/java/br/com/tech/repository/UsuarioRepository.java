d1bdc8b6f4a786a5add4de510300efdd85b3fc4b
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-24 01:31:18 -0300

    Create UsuarioRepository.java

---
+++ src/main/java/br/com/tech/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.com.tech.repository;
+
+import br.com.tech.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, UUID> {
+    Optional<UsuarioEntity> findByEmail(String email);
+    Optional<UsuarioEntity> findByUsuarioLogin(UUID usuario);
+}
